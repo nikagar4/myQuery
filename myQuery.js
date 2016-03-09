a529623a7e49f92cec231b7e6110c8ea9869e5bd
Nikoloz Garibashvili <nikagaribashvili@yahoo.com>
2016-03-09 18:09:59 +0400

    newer version of myQuery added

--- myQuery.js
+++ myQuery.js
@@ -1,14 +1,44 @@
-function getBy(selector) {
-	if (!selector) { return;}
-  var type = selector[0];
-  debugger;
-  var selector = selector.substring(1);
-  switch (type) {
-     case '.':
-				return document.getElementsByClassName(selector)[0];
-     case '#':
-        return document.getElementById(selector);
-     default:
-        return document.querySelector(type + selector);
-  }
-}
+var $ = function() {
+  var publicAPI = {
+    getByClass: function(selector) {
+      var type = selector[0];
+      if (!selector || type !== '.') {
+        return;
+      }
+      return document.getElementsByClassName(selector.substring(1));
+    },
+    getById: function(selector) {
+      var type = selector[0];
+      if (!selector || type !== '#') {
+        return;
+      }
+      return document.getElementById(selector.substring(1));
+    },
+    getByAttribute: function(selector) {
+      if (!selector) {
+        return;
+      }
+      return document.querySelector(selector);
+    },
+    ready: function(func) {
+      window.onload = func;
+    },
+    event: function(elem, type, handler) {
+      elem.addEventListener(type, function() {
+        handler();
+      });
+    }
+  };
+  return publicAPI;
+}();
+$.getById('#output').innerHTML = $.getById('#span1');
+$.ready(function() {
+  console.log('hello world');
+});
+$.event($.getById('#span1'), 'click', function() {
+  alert('hello world');
+});
+
+// Current Code Written by Shako Kalandarishvili, Ideas by Nika and Shako
+// Commits made by Nika
+// Couldn't find the old version
